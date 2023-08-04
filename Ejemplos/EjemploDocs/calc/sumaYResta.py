ed77d35c7b80e5c1e64e547df19c6afeef884c2b
artuzam <charliecazc@gmail.com>
2023-08-03 18:58:57 -0600

    Ejemplo docs

---
+++ Ejemplos/EjemploDocs/calc/sumaYResta.py
@@ -0,0 +1,26 @@
+def suma(a, b):
+    """
+    Realiza la suma de dos números enteros.
+
+    Parámetros:
+        a (int): El primer número entero.
+        b (int): El segundo número entero.
+
+    Retorna:
+        int: La suma de los dos números.
+    """
+    return a + b
+
+
+def resta(a, b):
+    """
+    Realiza la resta de dos números enteros.
+
+    Parámetros:
+        a (int): El número del que se va a restar.
+        b (int): El número que se va a restar.
+
+    Retorna:
+        int: El resultado de la resta.
+    """
+    return a - b
---
+++ Ejemplos/EjemploDocs/docs/sumaYResta.rst
@@ -0,0 +1,8 @@
+sumaYResta module
+=================
+Clase que se encarga de hacer sumas y restas
+
+.. automodule:: sumaYResta
+   :members:
+   :undoc-members:
+   :show-inheritance:
